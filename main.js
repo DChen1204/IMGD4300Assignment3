c75d8a11d416b2c06cd9a597d0528f2cc3dd86ad
Diwen <dchen5@wpi.edu>
2026-03-29 11:03:37 -0400

    No camera for now

--- main.js
+++ main.js
@@ -15,62 +15,43 @@ const fragmentShader = `
 // the sampler / texture for live video appear
 // after all uniforms, storage buffers, and the
 // sampler / texture for feedback.
-@group(0) @binding(0) var<uniform> resolution: vec2f;
-@group(0) @binding(1) var backSampler:    sampler;
-@group(0) @binding(2) var backBuffer:     texture_2d<f32>;
-@group(0) @binding(3) var videoSampler:   sampler;
+
+//@group(0) @binding(0) var<uniform> resolution: vec2f;
+//@group(0) @binding(1) var backSampler:    sampler;
+//@group(0) @binding(2) var backBuffer:     texture_2d<f32>;
+//@group(0) @binding(3) var videoSampler:   sampler;
 
 // NOTE THAT THERE IS A DIFFERENT GROUP NUMBER FOR THE
 // VIDEO TEXTURE BELOW. This lets gulls easily rebind
 // the texture for each frame, without having to rebind
 // the other variables in group 0. Given the new group,
 // the binding index resets to 0.
-@group(1) @binding(0) var videoBuffer:    texture_external;
+//@group(1) @binding(0) var videoBuffer:    texture_external;
 
 @fragment
 fn fs( @builtin(position) pos : vec4f ) -> @location(0) vec4f {
-   let p = pos.xy / resolution;
+   //let p = pos.xy / resolution;
 
   // WebGPU requires us to use a different function to sample
   // from live video / video files
-  let video = textureSampleBaseClampToEdge( videoBuffer, videoSampler, p );
+  //let video = textureSampleBaseClampToEdge( videoBuffer, videoSampler, p );
 
   // read the previous frame of video
-  let fb = textureSample( backBuffer, backSampler, p );
+ // let fb = textureSample( backBuffer, backSampler, p );
 
   // combine the circle and the feedback
-  let out = video * .05 + fb * .975;
+ // let out = video * .05 + fb * .975;
 
-  return vec4f( out.rgb, 1. );
+  // return vec4f( out.rgb, 1. );
+  return vec4f( 1.,0.,0. , 1. );
 }
 `
 
 // our vertex + fragment shader together
 const shader = quadVertexShader + fragmentShader
 
-// create a render pass
-//const renderPass = await sg.render({ shader })
+// create a render 
+const render = await sg.render({ shader })
 
-const resolution = [ window.innerWidth, window.innerHeight ]
+sg.run( render )
 
-const data = [
-  sg.uniform(resolution),
-  sg.sampler(),
-  sg.feedback(),
-  sg.sampler(),
-  sg.video(Video.element)
-]
-
-const renderPass = await sg.render({ shader: shader, data: data })
-
-// const resolution = [ window.innerWidth, window.innerHeight ]
-
-// run our render pass
-//sg.run( renderPass )
-sg.run( renderPass )
-
-//sg
-//.uniforms({resolution})
-//.textures([Video.element])
-//.render(shader)
-//.run()
